61a5ada4d14d5be408c6296746b3165bb1fca018
Danil Shashkov <shashkovdanil@gmail.com>
2017-06-12 19:42:03 +0500

    fix

--- src/main/scala/app/App.scala
+++ src/main/scala/app/App.scala
@@ -5,7 +5,7 @@ import app.components.Main
 import scala.scalajs.js.JSApp
 import org.scalajs.dom
 import dom.document
-import scalacss.Defaults._
+import scalacss.DevDefaults._
 import scalacss.ScalaCssReact._
 
 object App extends JSApp {
